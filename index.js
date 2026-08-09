4070b2580982bcb4cf659d349125df42907fac2c
VikaSh221525 <vikassharma221525@gmail.com>
2026-08-09 23:11:26 +0530

    Testing Working

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World! v1');
+    res.status(200).json({
+        message: "Hello i am vikash developed my first CI/CD pipe line via GithubActions"
+    })
 });
 
 app.listen(PORT, () => {
